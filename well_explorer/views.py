069c471bfe38df2c5d59eb8a403b8210b6ae3da4
Clement Helsens <clementhelsens>
2026-01-22 16:48:32 +0100

    add categories

--- well_explorer/views.py
+++ well_explorer/views.py
@@ -1152,7 +1152,11 @@ def vast_handler(doc: bokeh.document.Document) -> None:
     norm_layout = bokeh.layouts.column(bokeh.layouts.row(indent,bokeh.layouts.column(dropdown_exp, well_mapping_button, create_training_button), bokeh.models.Spacer(width=20),    bokeh.layouts.column(image_message,drug_message)),
                                        bokeh.layouts.Spacer(width=50),
                                        bokeh.layouts.row(indent,  bokeh.layouts.column(plot_wellplate_dest, plot_wellplate_dest_2),
-                                                         bokeh.layouts.column(bokeh.layouts.row(bokeh.layouts.Spacer(width=10), bokeh.layouts.column(contrast_slider,predict_button, use_corrected_checkbox), bokeh.layouts.column(bokeh.layouts.row(dropdown_total_somites, dropdown_total_somites_err,dropdown_good_image), bokeh.layouts.row(dropdown_bad_somites_cat1, dropdown_bad_somites_cat2, dropdown_bad_somites_cat3, dropdown_good_orientation),bokeh.layouts.column(saveimages_button ,images_comments)),
+                                                         bokeh.layouts.column(bokeh.layouts.row(bokeh.layouts.Spacer(width=10), 
+                                                                                                bokeh.layouts.column(contrast_slider,predict_button, use_corrected_checkbox), 
+                                                                                                bokeh.layouts.column(bokeh.layouts.row(dropdown_total_somites, dropdown_total_somites_err,dropdown_good_image), 
+                                                                                                                     bokeh.layouts.row(dropdown_bad_somites_cat1, dropdown_bad_somites_cat2, dropdown_bad_somites_cat3, dropdown_good_orientation)),
+                                                                                                bokeh.layouts.column(saveimages_button ,images_comments)),
                                                                               bokeh.layouts.row(prediction_message),
                                                                               bokeh.layouts.row(plot_img_bf, bokeh.layouts.Spacer(width=10),plot_img_yfp),
                                                                               bokeh.layouts.row(plot_img_vast))))
